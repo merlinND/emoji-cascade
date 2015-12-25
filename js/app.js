04d593c8474d9d5ac1e2fd550e7695c910017f02
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-12-25 15:32:25 +0100

    Add texture with async loading

--- js/app.js
+++ js/app.js
@@ -26,8 +26,36 @@ var setColor = function(gl, program, color) {
   gl.uniform4fv(colorAttribute, color);
 }
 
+var loadTextureAsync = function(gl, path, placeholderColor, cb) {
+  // Create a texture
+  var texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  // Fill the texture with a solid color while waiting for the image to load
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+                new Uint8Array(placeholderColor));
+  // Asynchronously load an image
+  var image = new Image();
+  image.src = path;
+  image.addEventListener('load', function() {
+    // Now that the image has loaded make copy it to the texture.
+    console.log("Successfully loaded texture from: " + path + " (" + image.width + " x " + image.height + ")");
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
+
+    // TODO: make image size a power of 2 so that we can geretate mipmaps
+    var isPowerOf2 = function(n) { return (n & (n - 1)) == 0; };
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+      gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+      console.warn("Could not generate mipmaps for texture.");
+    }
+
+    requestAnimationFrame(cb);
+  });
+}
+
 // Allocate and fill a GL buffer with vertices
-var makeBufferForRectangle = function(gl, attribute) {
+var makeBufferForVertices = function(gl, attribute) {
   var nTriangles = 2;
 
   var buffer = gl.createBuffer();
@@ -38,7 +66,8 @@ var makeBufferForRectangle = function(gl, attribute) {
 
   return buffer;
 }
-var fillRectangleBuffer = function(gl, coordinates) {
+var fillRectangleBuffer = function(gl, buffer, coordinates) {
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(
       gl.ARRAY_BUFFER,
       new Float32Array(coordinates),
@@ -46,22 +75,59 @@ var fillRectangleBuffer = function(gl, coordinates) {
     );
 }
 
-var drawRectangle = function(gl, x, y, width, height) {
-  var verticesCoordinates = [
-      x,         y,
-      x,         y + height,
-      x + width, y,
-      x + width, y + height,
-      x        , y + height,
-      x + width, y,
-    ];
+// Allocate and fill a GL buffer for texture coordinates
+var makeBufferForTextureCoordinates = function(gl, attribute) {
+  var buffer = gl.createBuffer();
 
-  // Draw triangles
-  fillRectangleBuffer(gl, verticesCoordinates);
-  gl.drawArrays(gl.TRIANGLES, 0, verticesCoordinates.length / 2);
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.enableVertexAttribArray(attribute);
+  gl.vertexAttribPointer(attribute, 2, gl.FLOAT, false, 0, 0);
+
+  return buffer;
+}
+var fillTextureCoordinatesBuffer = function(gl, buffer, coordinates) {
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array(coordinates),
+      gl.STATIC_DRAW
+    );
+}
+
+var drawRectangle = function(vertexBuffer, uvBuffer) {
+  return function(gl, x, y, width, height) {
+    var verticesCoordinates = [
+        x,         y,
+        x,         y + height,
+        x + width, y,
+        x + width, y + height,
+        x        , y + height,
+        x + width, y,
+      ];
+    var textureCoordinates = [
+      0, 0,
+      0, 1,
+      1, 0,
+      1, 1,
+      0, 1,
+      1, 0,
+    ]
+
+    // Draw triangles
+    fillRectangleBuffer(gl, vertexBuffer, verticesCoordinates);
+    fillTextureCoordinatesBuffer(gl, uvBuffer, textureCoordinates);
+    gl.drawArrays(gl.TRIANGLES, 0, verticesCoordinates.length / 2);
+  }
 }
 
 var drawRectangleGrid = function(gl, program, nx, ny, spacing, size) {
+  var vertexBuffer = makeBufferForVertices(gl,
+                                           gl.getAttribLocation(program, 'a_position'));
+
+  var uvBuffer = makeBufferForTextureCoordinates(gl,
+                                                 gl.getAttribLocation(program, "a_texcoords"));
+
+  var draw = drawRectangle(vertexBuffer, uvBuffer);
   for (var i = 0; i < nx; ++i) {
     for (var j = 0; j < ny; ++j) {
       var color = [Math.random(), Math.random(), Math.random(), 1];
@@ -69,7 +135,7 @@ var drawRectangleGrid = function(gl, program, nx, ny, spacing, size) {
           y = (size + spacing) * j;
 
       setColor(gl, program, color);
-      drawRectangle(gl, x, y, size, size, color);
+      draw(gl, x, y, size, size, color);
     }
   }
 }
@@ -83,13 +149,18 @@ var main = function() {
 
   setResolution(gl, program, canvasId);
 
-  var positionAttribute = gl.getAttribLocation(program, 'a_position');
-  makeBufferForRectangle(gl, positionAttribute);
+  var draw = function() {
+    var nRectanglesX = 10;
+    var nRectanglesY = nRectanglesX;
+    var spacing = canvas.width / 50;
+    drawRectangleGrid(gl, program, nRectanglesX, nRectanglesY, spacing, canvas.width / nRectanglesX);
+  }
 
-  var nRectanglesX = 10;
-  var nRectanglesY = nRectanglesX;
-  var spacing = canvas.width / 50;
-  drawRectangleGrid(gl, program, nRectanglesX, nRectanglesY, spacing, canvas.width / nRectanglesX);
+  draw();
+
+  // var texturePath = 'http://127.0.0.1:8000/textures/sheet_apple_16.png';
+  var texturePath = 'http://127.0.0.1:8000/textures/doge.jpg';
+  loadTextureAsync(gl, texturePath, [0, 0, 255, 255], draw);
 }
 
 main();
